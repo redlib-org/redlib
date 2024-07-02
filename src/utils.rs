67a890cab30e899650d40aa5c3d5416d3958c723
Matthew Esposito <matt@matthew.science>
2024-07-02 08:04:27 -0400

    fix(posts): fix sort call on new (#171)

--- src/utils.rs
+++ src/utils.rs
@@ -425,8 +425,6 @@ impl Post {
 				ws_url: val(post, "websocket_url"),
 			});
 		}
-		posts.sort_by(|a, b| b.created_ts.cmp(&a.created_ts));
-		posts.sort_by(|a, b| b.flags.stickied.cmp(&a.flags.stickied));
 		Ok((posts, res["data"]["after"].as_str().unwrap_or_default().to_string()))
 	}
 }
