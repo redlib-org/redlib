b539904275aea83bbe481a892c3d825694514f70
Martin Lindhe <martin@ubique.se>
2025-01-24 18:07:09 +0100

    rss: proxy links in users and subreddit feeds, fixes #359

--- src/user.rs
+++ src/user.rs
@@ -163,7 +163,7 @@ pub async fn rss(req: Request<Body>) -> Result<Response<Body>, String> {
 				.into_iter()
 				.map(|post| Item {
 					title: Some(post.title.to_string()),
-					link: Some(utils::get_post_url(&post)),
+					link: Some(format_url(&utils::get_post_url(&post))),
 					author: Some(post.author.name),
 					content: Some(rewrite_urls(&post.body)),
 					..Default::default()
