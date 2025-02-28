d097495a4182d0c9ba52ad9833171b2cab6d1a22
Matthew Esposito <matt@matthew.science>
2025-02-27 20:10:01 -0500

    fix: handle case insensitivity for subs

--- src/search.rs
+++ src/search.rs
@@ -71,8 +71,12 @@ pub async fn find(req: Request<Body>) -> Result<Response<Body>, String> {
 		return Ok(redirect("/"));
 	}
 
-	if query.starts_with("r/") || query.starts_with("R/") || query.starts_with("user/") {
+	if query.starts_with("r/") || query.starts_with("user/") {
 		return Ok(redirect(&format!("/{query}")));
+	}
+	
+	if query.starts_with("R/") {
+		return Ok(redirect(&format!("/r{}", &query[1..])));
 	}
 
 	if query.starts_with("u/") || query.starts_with("U/") {
